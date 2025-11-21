c16d4c10b6c9c2bac907948114671e08142f1313
Hasib <gudhulibela031@gmail.com>
2025-11-21 16:11:20 +0600

    Add files via upload

---
+++ 2.Square_of_a_Floating_Number.cpp
@@ -0,0 +1,13 @@
+/*2. Square of a Floating Number
+Input: 3.5
+Output: Square: 12.25 */
+#include <iostream>
+using namespace std;
+int main()
+{
+float n;
+cout<<"Enter a number :"<<endl;
+cin>>n;
+cout<<"square of "<<n<<" is "<<n*n<<endl;
+return 0;
+}
