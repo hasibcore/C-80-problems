307963c68893dbd0b2acdc3c3ce70f5e8525b1df
Hasib <gudhulibela031@gmail.com>
2025-11-21 12:39:05 +0600

    Add files via upload

---
+++ Add_Two_Integers.cpp
@@ -0,0 +1,18 @@
+/* 1. Add Two Integer
+Input: 15 27
+Output: Sum: 42 */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    float x,y,sum;
+   
+    cout<<"Add Two Numbers "<<endl;
+     cin>>x>>y;
+sum=x+y;
+cout<<"Sum: "<<sum; 
+return 0;
+}
+
